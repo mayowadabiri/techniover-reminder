0cd9f05aad60cae039f7a53c6fdb32d4e2e9dcdf
Mayowa dabiri <mayowad43@gmail.com>
2023-04-18 12:55:16 +0100

    updated config file

--- config/config.ts
+++ config/config.ts
@@ -9,17 +9,11 @@ const config: { [key: string]: any } = {
     dialect: 'postgres',
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'postgres',
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     dialect: 'postgres',
   },
 };
